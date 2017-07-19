07187b431a161180964d231039124b259779262e
Abhishek gurjar <abhishek.gurjar247@gmail.com>
2017-07-19 15:52:38 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,71 +1,94 @@
-var auto = document.getElementById('auto')
-var autores = document.getElementById('autores')
-var tempVal = ''
-Object.assign(autores.style,{listStyle:'none',width:'100%',position:'absolute',top:'13px',right:'-10px'});
-auto.addEventListener('keyup', function(){
-	//console.log(document.getElementById('auto').style.left)
-	tempVal = auto.value
-	while (autores.hasChildNodes()) {   
-		autores.removeChild(autores.firstChild);
-	}
-	while(!auto.value){
-		autores.removeChild(autores.firstChild);
-	}
+document.addEventListener("DOMContentLoaded", function(event) { 
+	function automata (id,data){
+		var auto = document.getElementById(id)
+		var val = data;
+		autores = document.createElement('ul')
+		autores.setAttribute('id','autores')
+		auto.parentNode.insertBefore(autores, auto.nextSibling)
 
-	var val = ['test1','test2','test3','abhishek','gurjar']
+		var tempVal = ''
+		var leftval = auto.getBoundingClientRect().left
+		var topval = auto.getBoundingClientRect().top
 
-	function matcher(match) {
-		if (match.includes(auto.value)){
-			return match
-		}
-	}
+		topval = topval + 5;
+		topval = topval+'px';
+		leftval = leftval+'px';
 
-	res = val.filter(matcher);
-	for(let i = 0; i < res.length; i++){
-		var li = document.createElement('li')
-		li.setAttribute('id','autolist')
-		li.setAttribute('tabindex',0)
-		li.setAttribute('onkeyup','rotate(this)')
-		li.setAttribute('onclick','select(this.innerHTML)')
-		Object.assign(li.style,{border:'1px solid',width:'171px',height:'15'});
-		li.innerHTML = res[i]
-		autores.appendChild(li)
+		if(auto.style.width == "" && auto.style.height == ""){
+			var liwidth = '171px';
+			var liheight = '18px';
+		}
+		else{
+			var liwidth = auto.style.width;
+			var liheight = auto.style.height;
+		}
+
+		Object.assign(autores.style,{listStyle:'none',width:'100%',position:'absolute',top:topval,left:leftval,padding:'0px'});
+		auto.addEventListener('keyup', function(){
+			//console.log(document.getElementById('auto').style.left)
+			window.tempVal = auto.value
+
+			while (autores.hasChildNodes()) { 
+			autores.removeChild(autores.firstChild);
+			}
+			while(auto.value == ''){
+				autores.removeChild(autores.firstChild);
+			}
+
+			function matcher(match) {
+			if (match.includes(auto.value)){
+				return match
+			}
+			}
+
+			res = val.filter(matcher);
+			for(let i = 0; i < res.length; i++){
+				var li = document.createElement('li')
+				li.setAttribute('id','autolist')
+				li.setAttribute('tabindex',0)
+				li.setAttribute('onkeyup','automata.rotate(this)')
+				li.setAttribute('onclick','automata.select(this.innerHTML)')
+				Object.assign(li.style,{border:'1px solid',width:liwidth,height:liheight});
+				li.innerHTML = res[i]
+				autores.appendChild(li)
+			}
+			auto.addEventListener('keydown',function(e){
+				if(e.keyCode == 40){
+					autores.firstChild.focus()
+					auto.value = autores.firstChild.innerHTML
+				}
+			})
+		})					
 	}
-	auto.addEventListener('keydown',function(e){
-		if(e.keyCode == 40){
-			autores.firstChild.focus()
-			auto.value = autores.firstChild.innerHTML
-		}
-	})
-})
-function rotate(y){
-	y.addEventListener('keydown',function(e){
-		if(e.keyCode == 40 && y.nextSibling){
-			y.nextSibling.focus()
-			auto.value = y.nextSibling.innerHTML
-		}
-		else if(e.keyCode == 38){
-			if(!y.previousSibling){
-				auto.value = tempVal
-				auto.focus()
-				auto.value = auto.value
+	automata.rotate = function(y){
+		y.addEventListener('keydown',function(e){
+			if(e.keyCode == 40 && y.nextSibling){
+				y.nextSibling.focus()
+				auto.value = y.nextSibling.innerHTML
 			}
-			else{
-				y.previousSibling.focus()
-				auto.value = y.previousSibling.innerHTML
+			else if(e.keyCode == 38){
+				if(!y.previousSibling){
+					auto.value = window.tempVal
+					auto.focus()
+					auto.value = auto.value
+				}
+				else{
+					y.previousSibling.focus()
+					auto.value = y.previousSibling.innerHTML
+				}
 			}
-		}
-		else if(e.keyCode == 13){
-			auto.value = y.innerHTML
-			while(auto.value == y.innerHTML){
-				autores.removeChild(autores.firstChild);	
+			else if(e.keyCode == 13){
+				auto.value = y.innerHTML
+				while(auto.value == y.innerHTML){
+					autores.removeChild(autores.firstChild);	
+				}
 			}
+		})
+	}	
+	automata.select = function(x){
+		auto.value = x
+		while(auto.value == x){
+			autores.removeChild(autores.firstChild);	
 		}
-	})
-}
-function select(x){
-	auto.value = x
-	while(auto.value == x){
-		autores.removeChild(autores.firstChild);	
 	}
-}
+});
